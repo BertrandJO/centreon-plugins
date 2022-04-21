33e81d9c21f8f3d7f87ea5739ceda5fa3cb3e849
garnier-quentin <garnier.quentin@gmail.com>
2022-04-21 16:36:17 +0200

    wip

--- storage/purestorage/restapi/custom/api.pm
+++ storage/purestorage/restapi/custom/api.pm
@@ -23,7 +23,9 @@ package storage::purestorage::restapi::custom::api;
 use strict;
 use warnings;
 use centreon::plugins::http;
-use JSON;
+use centreon::plugins::statefile;
+use JSON::XS;
+use Digest::MD5;
 
 sub new {
     my ($class, %options) = @_;
@@ -44,7 +46,10 @@ sub new {
             'hostname:s' => { name => 'hostname' },
             'username:s' => { name => 'username' },
             'password:s' => { name => 'password' },
+            'token:s'    => { name => 'token' },
             'timeout:s'  => { name => 'timeout' },
+            'port:s'     => { name => 'port' },
+            'proto:s'    => { name => 'proto' },
             'api-path:s' => { name => 'api_path' },
             'unknown-http-status:s'  => { name => 'unknown_http_status' },
             'warning-http-status:s'  => { name => 'warning_http_status' },
@@ -55,6 +60,7 @@ sub new {
 
     $self->{output} = $options{output};
     $self->{http} = centreon::plugins::http->new(%options);
+    $self->{cache} = centreon::plugins::statefile->new(%options);
     
     return $self;
 }
@@ -70,24 +76,36 @@ sub set_defaults {}
 sub check_options {
     my ($self, %options) = @_;
 
-    $self->{hostname}   = (defined($self->{option_results}->{hostname})) ? $self->{option_results}->{hostname} : undef;
-    $self->{username}   = (defined($self->{option_results}->{username})) ? $self->{option_results}->{username} : undef;
-    $self->{password}   = (defined($self->{option_results}->{password})) ? $self->{option_results}->{password} : undef;
-    $self->{timeout}    = (defined($self->{option_results}->{timeout})) ? $self->{option_results}->{timeout} : 10;
-    $self->{api_path}   = (defined($self->{option_results}->{api_path})) ? $self->{option_results}->{api_path} : '/api/1.11';
+    $self->{hostname}     = (defined($self->{option_results}->{hostname})) ? $self->{option_results}->{hostname} : '';
+    $self->{api_username} = (defined($self->{option_results}->{username})) ? $self->{option_results}->{username} : '';
+    $self->{api_password} = (defined($self->{option_results}->{password})) ? $self->{option_results}->{password} : '';
+    $self->{timeout}      = (defined($self->{option_results}->{timeout})) ? $self->{option_results}->{timeout} : 30;
+    $self->{port}         = (defined($self->{option_results}->{port})) ? $self->{option_results}->{port} : 80;
+    $self->{proto}        = (defined($self->{option_results}->{proto})) ? $self->{option_results}->{proto} : 'http';
     $self->{unknown_http_status} = (defined($self->{option_results}->{unknown_http_status})) ? $self->{option_results}->{unknown_http_status} : '%{http_code} < 200 or %{http_code} >= 300';
     $self->{warning_http_status} = (defined($self->{option_results}->{warning_http_status})) ? $self->{option_results}->{warning_http_status} : '';
     $self->{critical_http_status} = (defined($self->{option_results}->{critical_http_status})) ? $self->{option_results}->{critical_http_status} : '';
+    $self->{token} = $self->{option_results}->{token};
  
-    if (!defined($self->{hostname})) {
+    if ($self->{hostname} eq '') {
         $self->{output}->add_option_msg(short_msg => "Need to specify hostname option.");
         $self->{output}->option_exit();
     }
-    if (!defined($self->{username})) {
+
+    $self->{cache}->check_options(option_results => $self->{option_results});
+
+    if (defined($self->{token}) && $self->{token} ne '') {
+        $self->{api_path} = (defined($self->{option_results}->{api_path})) ? $self->{option_results}->{api_path} : '/api/2.0';
+        $self->{cache}->check_options(option_results => $self->{option_results});
+        return 0;
+    }
+
+    $self->{api_path} = (defined($self->{option_results}->{api_path})) ? $self->{option_results}->{api_path} : '/api/1.11';
+    if ($self->{api_username} eq '') {
         $self->{output}->add_option_msg(short_msg => "Need to specify username option.");
         $self->{output}->option_exit();
     }
-    if (!defined($self->{password})) {
+    if ($self->{api_password} eq '') {
         $self->{output}->add_option_msg(short_msg => "Need to specify password option.");
         $self->{output}->option_exit();
     }
@@ -113,21 +131,26 @@ sub build_options_for_httplib {
 sub settings {
     my ($self, %options) = @_;
 
-    $self->build_options_for_httplib();
+    return if (defined($self->{settings_done}));
     $self->{http}->add_header(key => 'Accept', value => 'application/json');
-    $self->{http}->add_header(key => 'Content-Type', value => 'application/json');
-    if (defined($self->{session_id})) {
-        $self->{http}->add_header(key => 'Cookie', value => 'session=' . $self->{session_id});
-    }
     $self->{http}->set_options(%{$self->{option_results}});
+    $self->{settings_done} = 1;
 }
 
-sub request_api {
+sub clean_token {
+    my ($self, %options) = @_;
+
+    my $datas = { updated => time() };
+    $self->{cache}->write(data => $datas);
+}
+
+sub request_auth_api {
     my ($self, %options) = @_;
 
     my $content = $self->{http}->request(
         method => $options{method},
         url_path => $options{url_path},
+        header => $options{header},
         query_form_post => $options{query_form_post},
         unknown_status => $self->{unknown_http_status},
         warning_status => $self->{warning_http_status},
@@ -136,7 +159,7 @@ sub request_api {
 
     my $decoded;
     eval {
-        $decoded = decode_json($content);
+        $decoded = JSON::XS->new->utf8->decode($content);
     };
     if ($@) {
         $self->{output}->add_option_msg(short_msg => "Cannot decode json response");
@@ -146,22 +169,22 @@ sub request_api {
     return $decoded;
 }
 
-sub get_api_token {
+sub get_api_token_v1 {
     my ($self, %options) = @_;
     
-    my $json_request = { username => $self->{username}, password => $self->{password} };
+    my $json_request = { username => $self->{api_username}, password => $self->{api_password} };
     my $encoded;
     eval {
-        $encoded = encode_json($json_request);
+        $encoded = JSON::XS->new->utf8->encode($json_request);
     };
     if ($@) {
         $self->{output}->add_option_msg(short_msg => "Cannot encode json request");
         $self->{output}->option_exit();
     }
 
-    $self->settings();
-    my $decoded = $self->request_api(
+    my $decoded = $self->request_auth_api(
         method => 'POST',
+        header => ['Content-Type: application/json'],
         url_path => $self->{api_path} . '/auth/apitoken',
         query_form_post => $encoded
     );
@@ -173,21 +196,25 @@ sub get_api_token {
     return $decoded->{api_token};
 }
 
-sub get_session {
+sub get_session_v1 {
     my ($self, %options) = @_;
     
     my $json_request = { api_token => $options{api_token} };
     my $encoded;
     eval {
-        $encoded = encode_json($json_request);
+        $encoded = JSON::XS->new->utf8->encode($json_request);
     };
     if ($@) {
         $self->{output}->add_option_msg(short_msg => "Cannot encode json request");
         $self->{output}->option_exit();
     }
 
-    $self->settings();
-    my $decoded = $self->request_api(method => 'POST', url_path => $self->{api_path} . '/auth/session', query_form_post => $encoded);
+    my $decoded = $self->request_auth_api(
+        method => 'POST',
+        header => ['Content-Type: application/json'],
+        url_path => $self->{api_path} . '/auth/session',
+        query_form_post => $encoded
+    );
     my ($cookie) = $self->{http}->get_header(name => 'Set-Cookie');
     if (!defined($cookie)) {
         $self->{output}->add_option_msg(short_msg => "Cannot get session");
@@ -198,31 +225,139 @@ sub get_session {
     return $1;
 }
 
-sub connect {
+sub credentials_v1 {
     my ($self, %options) = @_;
 
-    my $api_token = $self->get_api_token();
-    $self->{session_id} = $self->get_session(api_token => $api_token);
+    my $has_cache_file = $self->{cache}->read(statefile => 'purestorage_api_' . Digest::MD5::md5_hex($self->{hostname} . '_' . $self->{api_username}));
+    my $token = $self->{cache}->get(name => 'token');
+    my $session = $self->{cache}->get(name => 'session');
+    my $md5_secret_cache = $self->{cache}->get(name => 'md5_secret');
+    my $md5_secret = Digest::MD5::md5_hex($self->{api_username} . $self->{api_password});
+
+    if ($has_cache_file == 0 ||
+        !defined($token) || !defined($session) ||
+        (defined($md5_secret_cache) && $md5_secret_cache ne $md5_secret)
+        ) {
+        my $token = $self->get_api_token_v1();
+        my $session = $self->get_session_v1(api_token => $token);
+
+        my $datas = {
+            updated => time(),
+            token => $token,
+            session => $session,
+            md5_secret => $md5_secret
+        };
+        $self->{cache}->write(data => $datas);
+    }
+
+    return $session;
+}
+
+sub credentials_token_v2 {
+    my ($self, %options) = @_;
+
+    my $has_cache_file = $self->{cache}->read(statefile => 'purestorage_api_' . Digest::MD5::md5_hex($self->{hostname} . '_' . $self->{token}));
+    my $token = $self->{cache}->get(name => 'token');
+    my $md5_secret_cache = $self->{cache}->get(name => 'md5_secret');
+    my $md5_secret = Digest::MD5::md5_hex($self->{token});
+
+    if ($has_cache_file == 0 ||
+        !defined($token) ||
+        (defined($md5_secret_cache) && $md5_secret_cache ne $md5_secret)
+        ) {
+        my $content = $self->{http}->request(
+            method => 'POST',
+            query_form_post => '',
+            url_path => '/api/login',
+            header => [
+                'api-token: "' . $self->{token} . '"',
+                'Content-Type: application/json'
+            ]
+        );
+
+        $token = $self->{http}->get_header(name => 'X-Auth-Token');
+
+        if (!defined($token)) {
+            $self->{output}->add_option_msg(short_msg => 'Cannot get token');
+            $self->{output}->option_exit();
+        }
+        my $datas = {
+            updated => time(),
+            token => $token,
+            md5_secret => $md5_secret
+        };
+        $self->{cache}->write(data => $datas);
+    }
+
+    return $token;
+}
+
+sub credentials {
+    my ($self, %options) = @_;
+
+    my $creds = {};
+    if (defined($self->{token}) && $self->{token} ne '') {
+        my $token = $self->credentials_token_v2();
+        $creds = {
+            header => ['X-Auth-Token: ' . $token],
+            unknown_status => '',
+            warning_status => '',
+            critical_status => ''
+        };
+    } else {
+        my $session = $self->credentials_v1();
+        $creds = {
+            header => ['Cookie: session=' . $self->{sesssion}],
+            unknown_status => '',
+            warning_status => '',
+            critical_status => ''
+        };
+    }
+
+    return $creds;
 }
 
 sub get_object {
     my ($self, %options) = @_;
 
-    if (!defined($self->{api_token})) {
-        $self->connect();
-    }
-
     $self->settings();
-    return $self->request_api(method => 'GET', url_path => $self->{api_path} . $options{path});
-}
+    my $creds = $self->credentials();
+    my $content = $self->{http}->request(
+        method => 'GET',
+        url_path => $self->{api_path} . $options{path},
+        %$creds
+    );
 
-sub DESTROY {
-    my $self = shift;
-
-    if (defined($self->{session_id})) {
-        $self->request_api(method => 'DELETE', url_path => $self->{api_path} . '/auth/session');
+    # Maybe session is invalid. so we retry
+    if ($self->{http}->get_code() < 200 || $self->{http}->get_code() >= 300) {
+        $self->clean_token();
+        $creds = $self->credentials();
+        $content = $self->{http}->request(
+            method => 'GET',
+            url_path => $self->{api_path} . $options{path},
+            %$creds,
+            unknown_status => $self->{unknown_http_status},
+            warning_status => $self->{warning_http_status},
+            critical_status => $self->{critical_http_status}
+        );
     }
+
+    my $decoded;
+    eval {
+        $decoded = JSON::XS->new->utf8->decode($content);
+    };
+    if ($@) {
+        $self->{output}->add_option_msg(short_msg => 'Cannot decode json response');
+        $self->{output}->option_exit();
+    }
+    if ($decoded->{error} != 0) {
+        $self->{output}->add_option_msg(short_msg => "api error $decoded->{error}: " . $decoded->{message});
+        $self->{output}->option_exit();
+    }
+
+    return $decoded;
 }
+
 
 1;
 
@@ -244,6 +379,14 @@ Pure Storage Rest API custom mode
 
 Pure Storage hostname.
 
+=item B<--proto>
+
+Set protocol (default: 'http')
+
+=item B<--port>
+
+Set HTTP port (default: 80)
+
 =item B<--username>
 
 Pure Storage username.
@@ -252,9 +395,13 @@ Pure Storage username.
 
 Pure Storage password.
 
+=item B<--token>
+
+Use token authentication (api v2 required).
+
 =item B<--timeout>
 
-Set HTTP timeout in seconds (Default: '10').
+Set HTTP timeout in seconds (Default: 30).
 
 =item B<--api-path>
 
